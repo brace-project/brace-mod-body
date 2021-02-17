3b8270421eebff87dc43c4ae6e917a19fa2acf5f
Matthes <matthes@leuffen.de>
2021-02-17 15:23:13 +0100

    fix unrecognized content types

--- src/BodyMiddleware.php
+++ src/BodyMiddleware.php
@@ -18,11 +18,21 @@ class BodyMiddleware extends BraceAbstractMiddleware
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $this->app->define("body", new DiProducer(function (DiContainer $container, array $optParams = [], \ReflectionClass $class = null, bool $isArray = false) use ($request) {
+
+            $contentType = $request->getHeader("Content-type")[0];
+            if (in_array($contentType, ["application/x-www-form-urlencoded", "multipart/form-data"])) {
+                $arrayData = $request->getParsedBody();
+            } elseif (in_array ($contentType, ["application/json", "text/json"])) {
+                $arrayData = phore_json_decode($request->getBody()->getContents());
+            } else {
+                throw new \InvalidArgumentException("Invalid input content type: '$contentType'");
+            }
+            
             if ($isArray === true) {
-                return (array)$request->getParsedBody();
+                return (array)$arrayData;
             }
             if ($class !== null) {
-                return phore_hydrate((array)$request->getParsedBody(), $class->getName());
+                return phore_hydrate($arrayData, $class->getName());
             }
             return $request->getBody()->getContents();
         }));
