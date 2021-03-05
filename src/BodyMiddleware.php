1553581ba8dc22a4de89d6c35d7d064288966572
Matthes <matthes@leuffen.de>
2021-03-05 13:16:21 +0100

    update

--- src/BodyMiddleware.php
+++ src/BodyMiddleware.php
@@ -14,32 +14,51 @@ use Psr\Http\Server\RequestHandlerInterface;
 class BodyMiddleware extends BraceAbstractMiddleware
 {
 
+    /**
+     * @var ServerRequestInterface
+     */
+    public $request;
+
+    public function buildBody(DiContainer $container, array $optParams = [], \ReflectionClass $class = null, bool $isArray = false) {
+        $request = $this->request;
+        $contentType = $request->getHeader("Content-type")[0];
+        if (in_array($contentType, ["application/x-www-form-urlencoded", "multipart/form-data"])) {
+            $arrayData = $request->getParsedBody();
+        } elseif (in_array ($contentType, ["application/json", "text/json"])) {
+            $arrayData = phore_json_decode($request->getBody()->getContents());
+        } else {
+            if ($class === null && $isArray === false)
+                return $request->getBody()->getContents();
+            throw new \InvalidArgumentException("Invalid input content type: '$contentType'");
+        }
+
+        if ($isArray === true) {
+            return (array)$arrayData;
+        }
+        if ($class !== null) {
+            return phore_hydrate($arrayData, $class->getName());
+        }
+        return $request->getBody()->getContents();
+    }
+
+    public function buildQuery(DiContainer $container, array $optParams = [], \ReflectionClass $class = null, bool $isArray = false) {
+        $request = $this->request;
+        if ($isArray === true) {
+            return (array)$request->getQueryParams();
+        }
+        if ($class !== null) {
+            return phore_hydrate($this->request->getQueryParams(), $class->getName());
+        }
+        return $request->getUri()->getQuery();
+    }
+
+
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $this->app->define("body", new DiProducer(function (DiContainer $container, array $optParams = [], \ReflectionClass $class = null, bool $isArray = false) use ($request) {
-
-            
-
-            $contentType = $request->getHeader("Content-type")[0];
-            if (in_array($contentType, ["application/x-www-form-urlencoded", "multipart/form-data"])) {
-                $arrayData = $request->getParsedBody();
-            } elseif (in_array ($contentType, ["application/json", "text/json"])) {
-                $arrayData = phore_json_decode($request->getBody()->getContents());
-            } else {
-                if ($class === null && $isArray === false)
-                    return $request->getBody()->getContents();
-                throw new \InvalidArgumentException("Invalid input content type: '$contentType'");
-            }
-            
-            if ($isArray === true) {
-                return (array)$arrayData;
-            }
-            if ($class !== null) {
-                return phore_hydrate($arrayData, $class->getName());
-            }
-            return $request->getBody()->getContents();
-        }));
+        $this->request = $request;
+        $this->app->define("body", new DiProducer([$this, "buildBody"]));
+        $this->app->define("query", new DiProducer([$this, "buildQuery"]));
         return $handler->handle($request);
     }
 }
